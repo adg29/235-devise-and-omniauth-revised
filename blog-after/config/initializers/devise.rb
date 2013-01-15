3ce336166e9774ec852eeed740801958e8905b8a
Alan David Garcia <alandavidgarcia@gmail.com>
2013-01-14 19:26:12 -0500

    Singly omniauth experiment

--- blog-after/config/initializers/devise.rb
+++ blog-after/config/initializers/devise.rb
@@ -207,6 +207,7 @@ Devise.setup do |config|
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', :scope => 'user,public_repo'
   config.omniauth :twitter, ENV["TWITTER_CONSUMER_KEY"], ENV["TWITTER_CONSUMER_SECRET"]
+  config.omniauth :singly, ENV["SINGLY_CLIENT_ID"], ENV["SINGLY_CLIENT_SECRET"]
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
